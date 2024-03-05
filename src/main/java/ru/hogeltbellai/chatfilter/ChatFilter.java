a9f58e2f7566bedf3323522ee9de7b54e08585e0
HogeltBellai <77669576+HogeltBellai@users.noreply.github.com>
2024-03-05 21:32:52 +0300

    Основной коммит

---
+++ src/main/java/ru/hogeltbellai/chatfilter/ChatFilter.java
@@ -0,0 +1,17 @@
+package ru.hogeltbellai.chatfilter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatFilter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
